e3ac9b0de383e1a126f58a92e7cfcbbb4ed440d4
Sillydan1 <asger.gitz@hotmail.com>
2015-05-24 01:51:46 +0200

    Added a simple button script

---
+++ TeleKins-JailJam2015/Assets/MyStuff/Scripts/Button.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour 
+{
+    private bool open = false;
+
+    public bool IsOpen
+    {
+        get
+        {
+            return open;
+        }
+    }
+
+	void OnTriggerEnter(Collider intruder)
+    {
+        if(intruder.transform.tag == "Cube")
+        {
+            open = true;
+        }
+    }
+    void OnTriggerExit(Collider intruder)
+    {
+        if (intruder.transform.tag == "Cube")
+        {
+            open = false;
+        }
+    }
+}
