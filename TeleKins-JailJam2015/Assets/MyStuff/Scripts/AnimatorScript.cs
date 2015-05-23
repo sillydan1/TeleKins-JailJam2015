e30e84d6898e2296c445d18a9dc9074b5ae37173
unknown <emil2794@VID28PFH12.VID.net.local>
2015-05-24 00:26:47 +0200

    woop

---
+++ TeleKins-JailJam2015/Assets/MyStuff/Scripts/AnimatorScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (Animator))]
+public class AnimatorScript : MonoBehaviour {
+
+private Animator anim;
+private AnimatorStateInfo currentBaseState;			// a reference to the current state of the animator, used for base layer
+
+	// Use this for initialization
+	void Start () {
+	
+	anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		if (Input.GetMouseButtonDown(1)){
+			anim.SetBool("Pull", true);
+			anim.SetBool("Hover", true);
+		}
+		else {
+			anim.SetBool("Pull", false);
+		}
+		if (Input.GetMouseButtonUp(1)){
+			anim.SetBool("Hover", false);
+		}
+		if (Input.GetMouseButtonDown(0) && anim.GetBool("Hover") == true) {
+			anim.SetBool("Push", true);
+		}
+		else {
+			anim.SetBool("Push", false);
+		}
+
+		if (Input.GetMouseButtonDown(0)){
+			anim.SetBool("Push", true);
+		}
+		else {
+			anim.SetBool("Push", false);
+		}
+		if (Input.GetKeyDown(KeyCode.Space)){
+			anim.SetBool("Jump", true);
+		}
+	}
+}
