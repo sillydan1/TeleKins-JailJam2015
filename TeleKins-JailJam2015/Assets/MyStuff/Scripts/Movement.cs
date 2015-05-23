a3f9e1897b72117005b8f9e285a76a0fd474fef5
Sillydan1 <asger.gitz@hotmail.com>
2015-05-23 18:11:15 +0200

    First person controller

--- TeleKins-JailJam2015/Assets/MyStuff/Scripts/Movement.cs
+++ TeleKins-JailJam2015/Assets/MyStuff/Scripts/Movement.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 10;
+    private CharacterMotor motor;
+    private CharacterController controller;
+    private bool isJumping;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        motor = GetComponent<CharacterMotor>();
+    }
+    void Update()
+    {
+        float hori = Input.GetAxis("Horizontal");
+        float verti = Input.GetAxis("Vertical");
+        Vector3 moveDir = transform.forward * verti;
+        moveDir += transform.right * hori;
+        moveDir *= Time.deltaTime * moveSpeed;
+
+
+        motor.inputJump = Input.GetButton("Jump");
+        controller.Move(moveDir);
+        //motor.inputMoveDirection = moveDir;
+        
+    }
 }
---
+++ TeleKins-JailJam2015/Assets/MyStuff/Scripts/Test.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+    private float whatever = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        whatever += Time.deltaTime * 10;
+        transform.position = new Vector3(transform.position.x, Mathf.Sin(whatever), transform.position.z);
+	}
+}
